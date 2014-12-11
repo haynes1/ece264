fe70da183538f7cd89e9247f6b45ef989b0b9116
Alex Haynes <haynes1@purdue.edu>
2014-12-10 21:51:43 -0500

    it works, and there are no memory errors with my testing program

--- PA12/answer12.c
+++ PA12/answer12.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <pthread.h>
 #include "answer12.h"
+
+typedef struct ValueAndRange
+{
+	uint128 n;
+	uint128 start;
+	uint128 end;
+	int prime;
+}ValueAndRange;
 
 uint128 alphaTou128(const char * str){
 
@@ -38,21 +48,87 @@ char * u128ToString(uint128 value){
 		c--;
 	}
 	rev[i] = '\0';
+	free(str);
 	return rev;
 }
 
-int primalityTestParallel(uint128 value, int n_threads){
-/*
-	if (n % 2 == 0);
-	return 0;
-    long int max = floor(sqrt(n));
-    long int i;
-    for (i = 2; i < max; i++) 
-    {
-	if (n % ((2 * i) + 1) == 0) 
-	return 0;
-    }	
-    return 1;*/
+ValueAndRange * ValueAndRange_construct(uint128 value, uint128 start, uint128 end){
+	ValueAndRange * v = malloc(sizeof(ValueAndRange));
+	v->n = value;
+	v->start = start;
+	v->end = end;
+	v->prime = 0;
+	return v;
+}
+//returns 0 
+void * primeTest(void * t){
+	int i;
+	ValueAndRange * v = (ValueAndRange *)t;
+	//iterate through the range testing 
+	for (i = v->start; i < v->end; i++)
+	{
+		if ((v->n % i) == 0)//found a factor(therefore n isn't prime)
+		{
+			v->prime = 1;
+		}
+	}
+	return (void*)&v->prime;
+}
 
-	return 0;
+int primalityTestParallel(uint128 value, int n_threads){
+
+	int i;
+	int isprime;
+	if (value == 0)
+	{
+		printf("0 is neither prime nor non-prime\n");
+	}
+	else if (value == 1 || value == 2 || (value % 2) == 0)
+	{
+		isprime = 1;
+	}
+
+	else
+	{
+		ValueAndRange * valueandrange_array[n_threads];
+		uint128 square_root = (uint128)sqrt((double) value);
+		int start;
+		int end = 1;
+		int range = (square_root - 2) / n_threads;
+		int ret;
+		pthread_t * threads = malloc(sizeof(pthread_t) * n_threads);
+		//create and start all threads
+		for (i = 0; i < n_threads; i++)
+		{
+			start = end + 1;
+			end = start + range;
+			valueandrange_array[i] = ValueAndRange_construct(value, start, end);
+			ret = pthread_create(&threads[i], NULL, primeTest, valueandrange_array[i]);
+			if(ret != 0)
+			{
+				printf ("Create pthread error!\n");
+			}
+			isprime = valueandrange_array[i]->prime;
+		}
+		//join all threads
+		for (i = 0; i < n_threads; i++)
+		{
+			ret = pthread_join(threads[i], NULL);
+			if (ret != 0)
+			{
+				printf ("ERROR ; pthread_join () returns %d\n", ret);
+				return EXIT_FAILURE ;
+			}
+		}
+		//see if the number is prime and free everything
+		isprime = 0;
+		for (i = 0; i < n_threads; i++)
+		{
+			isprime += valueandrange_array[i]->prime;
+			free(valueandrange_array[i]);
+		}
+		free(threads);
+	}
+
+	return isprime;
 }
--- PA12/mytests12.c
+++ PA12/mytests12.c
@@ -11,10 +11,24 @@ int main (int argc, char * * argv)
 	const char * string = "24354657";
 	number = alphaTou128(string);
 	printf("string is: %s, number is: %li\n", string, (long)number);
-	printf("============================Testing u128ToString=======================\n");
+	printf("==========================Testing u128ToString========================\n");
 	uint128 n = 456789;
 	char * s = u128ToString(n);
 	printf("number %li converts to: %s\n", (long)n, s);
+	free(s);
+	printf("=====================Testing primalityTestParallel====================\n");
+	//prime: 337, not prime: 338
+	n = 939391;
+	int isprime;
+	isprime = primalityTestParallel(n, 4);
+	if (isprime == 0)
+	{
+		printf("%li is prime\n", (long)n);
+	}
+	else
+	{
+		printf("%li is not prime\n", (long)n);
+	}
 
 
 	return EXIT_SUCCESS;
